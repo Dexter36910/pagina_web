39179f632448afe4ca2926662721feccc04327d8
Dexter36910 <david369za@gmail.com>
2026-06-10 00:12:02 -0600

    Update settings.py

--- proyecto_juego/settings.py
+++ proyecto_juego/settings.py
@@ -123,11 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
-    BASE_DIR.parent.parent / 'static',
-    
+    BASE_DIR / 'static',
 ]
+    
